aae520e2607758cc398fcae7031b64486d756144
datst <alualb932@ieselcaminas.org>
2022-11-15 23:06:43 +0100

    Initial commit

---
+++ src/Point.java
@@ -0,0 +1,15 @@
+public class Point {
+
+    // Fields4
+
+    public int x;
+    public int y;
+
+    // Constructors
+
+    public Point () {}
+    public Point (){
+        x= 0;
+        y= 0;
+        }
+}

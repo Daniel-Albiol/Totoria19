2a2225113f0fda7306487a738eb9158492cf27aa
datst <alualb932@ieselcaminas.org>
2022-12-19 18:31:51 +0100

    commit 1 13-12-22

---
+++ src/Rectangle.java
@@ -0,0 +1,10 @@
+public class Rectangle extends Point{
+    private int width;
+    private int height;
+
+    public Rectangle (){
+        super();
+        width = 0;
+        height = 0;
+    }
+}
